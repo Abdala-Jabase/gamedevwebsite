27c21e0e567be6ef195ad8ad770a5c9f4e8e9ee1
Abdala-Jabase <abdala.jabase@gmail.com>
2023-05-16 02:59:38 -0500

    Made website

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,19 @@
+// components/Header.js
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Welcome to Flee the Flood</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
