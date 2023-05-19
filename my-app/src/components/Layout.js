b54009a315bc8c233b4b9ffc1acc0a9f326fdc30
Abdala-Jabase <abdala.jabase@gmail.com>
2023-05-19 01:07:53 -0500

    Fixed title and logo

--- my-app/src/components/Layout.js
+++ my-app/src/components/Layout.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer"
+import { Helmet } from 'react-helmet';
 
 const Layout = () => {
     return (
         <>
+            <Helmet>
+                <title>AJ App Development Group</title>
+                <link rel="icon" type="image/png" href="../../titlelogo.png" />
+            </Helmet>
             <Header />
             <Outlet />
             <Footer />
