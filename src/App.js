ca358151663a0849162b6fea51e4e0ed6c41e09c
AJ <abdala.jabase@gmail.com>
2025-06-29 14:59:35 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
 
     <Routes>
-      <Route path="/gamedevwebsite" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="fleetheflood" element={<FleeTheFlood />} />
         <Route path="privacypolicy" element={<PrivacyPolicy />} />
